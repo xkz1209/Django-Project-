4760b87cbd462808ed56ef2eddfc28425afa1dd1
kax225 <kax225@bu.edu>
2024-09-21 15:58:41 -0400

    try

--- cs412/urls.py
+++ cs412/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("hw/", include("hw.url")),
-    path("quotes/",include("quotes.url"))
+    path("quotes/",include("quotes.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- quotes/urls.py
+++ quotes/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 #create a list of URLs for this app
 urlpatterns = [
-    path('',views.base, name="base"),        #our first , r for regular expressions matching
+    path(r'',views.base, name="base"),        #our first , r for regular expressions matching
     path('quote/', views.quote, name="quote"),
     path('show_all/', views.show_all, name="show_all"),
     path('about/', views.about, name="about")
