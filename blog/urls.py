cf4ee2f7c8f8d712e688ac0c5e37e6de4d225d86
kax225 <kax225@bu.edu>
2024-11-04 00:00:11 -0500

    u9

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from . import views
+from django.contrib.auth import views as auth_views ##new
 
 urlpatterns = [
   path(r'', views.RandomArticleView.as_view(), name='random'), #new
@@ -9,4 +10,8 @@ urlpatterns = [
   #path(r'create_comment', views.CreateCommentView.as_view(), name='create_comment'),
   path(r'article/<int:pk>/create_comment', views.CreateCommentView.as_view(),name='create_comment'),
   path(r'create_article',views.CreateArticleView.as_view(), name="create_article"),#new added
+  #authentication URLS:
+  path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'),name='login'),
+  path('logout/', auth_views.LogoutView.as_view(next_page='show_all'), name='logout'),
+  path('register/',views.RegistrationView.as_view(), name='register')
 ]
--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,20 +1,24 @@
 from django.urls import path
 from django.conf import settings
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
   path(r'profile/<int:pk>', views.ShowProfilePageView.as_view(), name='show_profile'),#new
   path('', views.ShowAllProfilesView.as_view(), name='show_all_profiles'), 
   path(r'create_profile/',views.CreateProfileView.as_view(),name='create_profile'),#new add
-  path(r'profile/<int:pk>/update/',views.UpdateProfileView.as_view(),name='update_profile'),#new add
-  path(r'profile/<int:pk>/create_status/',views.CreateStatusMessageView.as_view(),name='create_status'),#new add
+  path(r'profile/update/',views.UpdateProfileView.as_view(),name='update_profile'),#new add
+  path(r'profile/create_status/',views.CreateStatusMessageView.as_view(),name='create_status'),#new add
 
   path('status/<int:pk>/delete/', views.DeleteStatusMessageView.as_view(), name='delete_status'),
   path('status/<int:pk>/update/', views.UpdateStatusMessageView.as_view(), name='update_status'),
   
-  path('profile/<int:pk>/add_friend/<int:other_pk>/', views.CreateFriendView.as_view(), name='add_friend'),
-  path('profile/<int:pk>/friend_suggestions/', views.FriendSuggestionsView.as_view(), name='friend_suggestions'),
+  path('profile/add_friend/<int:other_pk>/', views.CreateFriendView.as_view(), name='add_friend'),
+  path('profile/friend_suggestions/', views.FriendSuggestionsView.as_view(), name='friend_suggestions'),
   
-  path('profile/<int:pk>/news_feed/', views.ShowNewsFeedView.as_view(), name='news_feed'),
+  path('profile/news_feed/', views.ShowNewsFeedView.as_view(), name='news_feed'),
+
+  path('login/', auth_views.LoginView.as_view(template_name='mini_fb/login.html'), name='login'),
+  path('logout/', auth_views.LogoutView.as_view(template_name='mini_fb/logged_out.html'), name='logout'),
 ]
