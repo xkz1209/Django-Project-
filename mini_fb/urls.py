a9dec936734a85f4891f2eb141ee9cb417bf67f8
kax225 <kax225@bu.edu>
2024-11-04 00:10:06 -0500

    modify

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
   path('', views.ShowAllProfilesView.as_view(), name='show_all_profiles'), 
   path(r'create_profile/',views.CreateProfileView.as_view(),name='create_profile'),#new add
   path(r'profile/update/',views.UpdateProfileView.as_view(),name='update_profile'),#new add
-  path(r'profile/create_status/',views.CreateStatusMessageView.as_view(),name='create_status'),#new add
+  path(r'status/create_status/',views.CreateStatusMessageView.as_view(),name='create_status'),#new add
 
   path('status/<int:pk>/delete/', views.DeleteStatusMessageView.as_view(), name='delete_status'),
   path('status/<int:pk>/update/', views.UpdateStatusMessageView.as_view(), name='update_status'),
