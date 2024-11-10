a882d556d1e2308d08f7efe67136bae8bc2f20a5
kax225 <kax225@bu.edu>
2024-11-10 05:32:12 -0500

    new

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -8,16 +8,8 @@ urlpatterns = [
   path(r'profile/<int:pk>', views.ShowProfilePageView.as_view(), name='show_profile'),#new
   path('', views.ShowAllProfilesView.as_view(), name='show_all_profiles'), 
   path(r'create_profile/',views.CreateProfileView.as_view(),name='create_profile'),#new add
-<<<<<<< HEAD
   path(r'profile/update/',views.UpdateProfileView.as_view(),name='update_profile'),#new add
   path(r'profile/create_status/',views.CreateStatusMessageView.as_view(),name='create_status'),#new add
-||||||| d33814b
-  path(r'profile/<int:pk>/update/',views.UpdateProfileView.as_view(),name='update_profile'),#new add
-  path(r'profile/<int:pk>/create_status/',views.CreateStatusMessageView.as_view(),name='create_status'),#new add
-=======
-  path(r'profile/update/',views.UpdateProfileView.as_view(),name='update_profile'),#new add
-  path(r'status/create_status/',views.CreateStatusMessageView.as_view(),name='create_status'),#new add
->>>>>>> a9dec936734a85f4891f2eb141ee9cb417bf67f8
 
   path('status/<int:pk>/delete/', views.DeleteStatusMessageView.as_view(), name='delete_status'),
   path('status/<int:pk>/update/', views.UpdateStatusMessageView.as_view(), name='update_status'),
